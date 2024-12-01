48e9674ffda6939cc3cef3092586a210ea8d840a
Zane Duffield <duffieldzane@gmail.com>
2024-12-01 22:59:05 +1100

    Optimise day 1 solution

--- src/day_01/mod.rs
+++ src/day_01/mod.rs
@@ -3,10 +3,21 @@ use std::cmp::Ordering;
 use itertools::Itertools;
 use nom::{character::complete::*, multi::*, sequence::separated_pair};
 
-pub type Input = Vec<(u32, u32)>;
+pub struct Input {
+    left: Vec<u32>,
+    right: Vec<u32>,
+}
 
 fn parse_input(input: &str) -> nom::IResult<&str, Input> {
-    separated_list0(line_ending, separated_pair(u32, space1, u32))(input)
+    let (input, lines) = separated_list0(line_ending, separated_pair(u32, space1, u32))(input)?;
+
+    Ok((
+        input,
+        Input {
+            left: lines.iter().map(|line| line.0).sorted_unstable().collect(),
+            right: lines.iter().map(|line| line.1).sorted_unstable().collect(),
+        },
+    ))
 }
 
 #[aoc_generator(day01)]
@@ -19,21 +30,19 @@ pub fn input_generator(input: &str) -> Input {
 #[aoc(day01, part1)]
 pub fn part_1(input: &Input) -> u32 {
     input
+        .left
         .iter()
-        .map(|elm| elm.0)
-        .sorted()
-        .zip(input.iter().map(|elm| elm.1).sorted())
-        .map(|(left, right)| left.abs_diff(right))
+        .zip(&input.right)
+        .map(|(left, right)| left.abs_diff(*right))
         .sum()
 }
 
 #[aoc(day01, part2)]
 pub fn part_2(input: &Input) -> u32 {
-    let left = input.iter().map(|elm| elm.0).sorted();
-    let mut right = input.iter().map(|elm| elm.1).sorted().peekable();
+    let mut right = input.right.iter().peekable();
 
     let mut sum = 0;
-    for l_num in left {
+    for l_num in &input.left {
         let _ = right.peeking_take_while(|r_num| *r_num < l_num).last();
         sum += l_num * right.clone().take_while(|r_num| *r_num == l_num).count() as u32;
     }
