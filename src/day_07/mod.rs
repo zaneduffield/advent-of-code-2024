40835f378564bd809110fdeb45e7e34d700ca82d
Zane Duffield <duffieldzane@gmail.com>
2024-12-08 08:59:48 +1100

    Optimise day 7

--- src/day_07/mod.rs
+++ src/day_07/mod.rs
@@ -1,4 +1,5 @@
 use nom::{bytes::complete::*, character::complete::*, multi::*, sequence::separated_pair};
+use num::Integer;
 
 pub struct Input {
     equations: Vec<Eq>,
@@ -21,65 +22,75 @@ pub fn input_generator(input: &str) -> Input {
     result
 }
 
-fn satisfiable_rec<const N: usize>(
+// By working backwards from the end we are able to prune the search tree because of divisibility constraints.
+// Specifically, if the last number doesn't divide the goal number, or (in the case of part 2)
+// the goal number doesn't 'end with' the last number, then we know that we've hit a dead end.
+// If we worked forward from the start, we would need to go all the way to the end to know whether a sequence
+// of operations results in the right number.
+fn satisfiable_rev<const N: usize>(
     eq: &Eq,
     next_pos: usize,
-    total: i64,
-    ops: [fn(i64, i64) -> i64; N],
+    goal: i64,
+    ops: [fn(i64, i64) -> Option<i64>; N],
 ) -> bool {
-    (next_pos >= eq.1.len() && eq.0 == total)
-        || next_pos < eq.1.len()
-            && ops
-                .iter()
-                .any(|op| satisfiable_rec(eq, next_pos + 1, op(total, eq.1[next_pos]), ops))
+    if next_pos == 0 {
+        return eq.1[0] == goal;
+    };
+
+    ops.iter().any(|op| {
+        op(goal, eq.1[next_pos])
+            .is_some_and(|next_goal| satisfiable_rev(eq, next_pos - 1, next_goal, ops))
+    })
 }
 
-fn satisfiable(eq: &Eq) -> bool {
-    satisfiable_rec(
-        eq,
-        1,
-        eq.1[0],
-        [|total, next| total * next, |total, next| total + next],
-    )
+fn remove_trailing_num(num: i64, on: i64) -> Option<i64> {
+    let digits = ((on as f64).log(10.0) + 1.0) as u32;
+
+    let (quot, rem) = num.div_rem(&10i64.pow(digits));
+    (rem == on).then_some(quot)
+}
+
+fn try_div(num: i64, denom: i64) -> Option<i64> {
+    let (quot, rem) = num.div_rem(&denom);
+    (rem == 0).then_some(quot)
+}
+
+pub fn solve<const N: usize>(input: &Input, ops: [fn(i64, i64) -> Option<i64>; N]) -> i64 {
+    input
+        .equations
+        .iter()
+        .filter(|eq| satisfiable_rev(eq, eq.1.len() - 1, eq.0, ops))
+        .map(|eq| eq.0)
+        .sum()
 }
 
 #[aoc(day7, part1)]
 pub fn part_1(input: &Input) -> i64 {
-    input
-        .equations
-        .iter()
-        .filter(|eq| satisfiable(eq))
-        .map(|eq| eq.0)
-        .sum()
-}
-
-fn satisfiable_part2(eq: &Eq) -> bool {
-    satisfiable_rec(
-        eq,
-        1,
-        eq.1[0],
-        [
-            |total, next| total * next,
-            |total, next| total + next,
-            |total, next| total * 10i64.pow(((next as f64).log(10.0) + 1.0) as u32) + next,
-        ],
-    )
+    solve(input, [try_div, |goal, next| Some(goal - next)])
 }
 
 #[aoc(day7, part2)]
 pub fn part_2(input: &Input) -> i64 {
-    input
-        .equations
-        .iter()
-        .filter(|eq| satisfiable_part2(eq))
-        .map(|eq| eq.0)
-        .sum()
+    solve(
+        input,
+        [remove_trailing_num, try_div, |goal, next| Some(goal - next)],
+    )
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use indoc::indoc;
+
+    #[test]
+    fn test_remove_trailing_num() {
+        assert_eq!(remove_trailing_num(12345, 6), None);
+        assert_eq!(remove_trailing_num(12345, 5), Some(1234));
+        assert_eq!(remove_trailing_num(12345, 45), Some(123));
+        assert_eq!(remove_trailing_num(12345, 44), None);
+        assert_eq!(remove_trailing_num(12345, 46), None);
+        assert_eq!(remove_trailing_num(12345, 345), Some(12));
+    }
 
     #[test]
     fn test() {
