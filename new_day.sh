4b7995c76368edae05eedddbbcbb1ad0b89222d9
Zane Duffield <duffieldzane@gmail.com>
2024-12-08 20:22:13 +1100

    Remove use of cargo-aoc in script

--- new_day.sh
+++ new_day.sh
@@ -9,6 +9,7 @@ mod_name="day_$(printf '%02d' "$1")"
 
 mkdir -p "$SRC/$mod_name"
 sed "s|xx|$day|g" "$SRC/day_xx/mod.rs" > "$SRC/$mod_name/mod.rs"
-sed -i -zE "s/(.*)(\naoc_lib.*)/\1pub mod $mod_name;\n\2/" "$SRC/lib.rs"
+printf "\npub mod $mod_name;" >> "$SRC/lib.rs"
 sed -i -zE 's/(.*)(\n\s*println!\("done.*)/\1    run_day_with_generator!('"$mod_name, \"$day\");\n\2/" "$SRC/bin/bin.rs"
-cargo aoc input -d "$day" -y 2024
+
+curl "https://adventofcode.com/2024/day/$day/input" --cookie "$(cat cookies)" > "input/2024/day$day.txt"
