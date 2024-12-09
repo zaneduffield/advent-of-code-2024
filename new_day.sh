b342fa43237cf564c393773dc03c9005950b53fa
Zane Duffield <duffieldzane@gmail.com>
2024-12-09 19:21:25 +1100

    Fix formatting

--- new_day.sh
+++ new_day.sh
@@ -9,7 +9,7 @@ mod_name="day_$(printf '%02d' "$1")"
 
 mkdir -p "$SRC/$mod_name"
 sed "s|xx|$day|g" "$SRC/day_xx/mod.rs" > "$SRC/$mod_name/mod.rs"
-printf "\npub mod $mod_name;" >> "$SRC/lib.rs"
+printf "pub mod $mod_name;\n" >> "$SRC/lib.rs"
 sed -i -zE 's/(.*)(\n\s*println!\("done.*)/\1    run_day_with_generator!('"$mod_name, \"$day\");\n\2/" "$SRC/bin/bin.rs"
 
 curl "https://adventofcode.com/2024/day/$day/input" --cookie "$(cat cookies)" > "input/2024/day$day.txt"
