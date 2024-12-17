b7c22ed3b5c9de886be9f1d35117a9f1469afa02
Zane Duffield <duffieldzane@gmail.com>
2024-12-17 14:17:49 +0800

    Solve day 13 part 2

--- src/day_13/mod.rs
+++ src/day_13/mod.rs
@@ -1,26 +1,32 @@
-use winnow::{ascii::*, combinator::*, token::*, Parser};
+use winnow::{ascii::*, combinator::*, Parser};
 
 pub type Input = Vec<Machine>;
 
 pub struct Machine {
-    a: (u32, u32),
-    b: (u32, u32),
-    prize: (u32, u32),
+    a: Pos,
+    b: Pos,
+    prize: Pos,
+}
+
+#[derive(Clone, Copy)]
+pub struct Pos {
+    x: i64,
+    y: i64,
 }
 
 fn parse_input(input: &mut &str) -> winnow::PResult<Input> {
     separated(
         0..,
         (
-            ("Button A: X+", dec_uint, ", Y+", dec_uint, line_ending),
-            ("Button B: X+", dec_uint, ", Y+", dec_uint, line_ending),
-            ("Prize: X=", dec_uint, ", Y=", dec_uint, line_ending),
+            ("Button A: X+", dec_int, ", Y+", dec_int, line_ending),
+            ("Button B: X+", dec_int, ", Y+", dec_int, line_ending),
+            ("Prize: X=", dec_int, ", Y=", dec_int, line_ending),
         )
             .map(
                 |((_, ax, _, ay, _), (_, bx, _, by, _), (_, px, _, py, _))| Machine {
-                    a: (ay, ax),
-                    b: (by, bx),
-                    prize: (py, px),
+                    a: Pos { x: ax, y: ay },
+                    b: Pos { x: bx, y: by },
+                    prize: Pos { x: px, y: py },
                 },
             ),
         line_ending,
@@ -32,30 +38,57 @@ pub fn input_generator(input: &str) -> Input {
     parse_input.parse(input).unwrap()
 }
 
-const A_COST: u32 = 3;
-const B_COST: u32 = 1;
+const A_COST: i64 = 3;
+const B_COST: i64 = 1;
 
-fn solve(m: &Machine) -> Option<u32> {
-    for n_a in 0..=100 {
-        for n_b in 0..=100 {
-            if n_a * m.a.0 + n_b * m.b.0 == m.prize.0 && n_a * m.a.1 + n_b * m.b.1 == m.prize.1 {
-                let cost = A_COST * n_a + B_COST * n_b;
-                return Some(cost);
-            }
+fn solve(m: &Machine) -> Option<u64> {
+    // consider the (vector) graph for N_A * A + N_B * B
+    // which we want to solve equal to P.
+    //
+    // If we write the equation using matrix multiplication we get
+    // |ax bx||na| = |px|
+    // |ay by||nb|   |py|
+    // so by finding the inverse of the left matrix we can get our solution.
+
+    let discriminant = m.a.x * m.b.y - m.b.x * m.a.y;
+
+    if discriminant == 0 {
+        None
+    } else {
+        let unscaled_inverse = [[m.b.y, -m.b.x], [-m.a.y, m.a.x]];
+
+        let n_a_unscaled = m.prize.x * unscaled_inverse[0][0] + m.prize.y * unscaled_inverse[0][1];
+        let n_b_unscaled = m.prize.x * unscaled_inverse[1][0] + m.prize.y * unscaled_inverse[1][1];
+
+        if n_a_unscaled % discriminant == 0 && n_b_unscaled % discriminant == 0 {
+            let cost =
+                A_COST * (n_a_unscaled / discriminant) + B_COST * (n_b_unscaled / discriminant);
+            (cost > 0).then_some(cost as u64)
+        } else {
+            None
         }
     }
-
-    None
 }
 
-pub fn part_1(input: &Input) -> u32 {
+pub fn part_1(input: &Input) -> u64 {
     input.iter().flat_map(solve).sum()
 }
 
-const OFFSET: u64 = 10000000000000;
+const OFFSET: i64 = 10000000000000;
 
-pub fn part_2(input: &Input) -> u32 {
-    0
+pub fn part_2(input: &Input) -> u64 {
+    input
+        .iter()
+        .map(|m| Machine {
+            a: m.a,
+            b: m.b,
+            prize: Pos {
+                x: m.prize.x + OFFSET,
+                y: m.prize.y + OFFSET,
+            },
+        })
+        .flat_map(|m| solve(&m))
+        .sum()
 }
 
 #[cfg(test)]
@@ -85,13 +118,13 @@ mod tests {
             "
         });
         assert_eq!(part_1(&input), 480);
-        // assert_eq!(part_2(&input),);
+        assert_eq!(part_2(&input), 875318608908);
     }
 
     #[test]
     fn test_my_input() {
         let input = input_generator(include_str!("../../input/2024/day13.txt"));
-        // assert_eq!(part_1(&input), );
-        // assert_eq!(part_2(&input),);
+        assert_eq!(part_1(&input), 31065);
+        assert_eq!(part_2(&input), 93866170395343);
     }
 }
